519f72854bb394a60990124367506991f2d1575e
albatrozzz <61119509+albatrozzz@users.noreply.github.com>
2021-11-29 12:29:21 +0200

    выполнен рефакторинг кода с использованием хуков

--- src/App.js
+++ src/App.js
@@ -4,56 +4,65 @@ import Statistics from "./components/Statistics/Statistics"
 import Section from "./components/Section/Section"
 
 function App(){
-  state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
-}
 
 const [good, setGood] = useState(0)
-const [neautral, setNeutral] = useState(0)
+const [neutral, setNeutral] = useState(0)
 const [bad, setBad] = useState(0)
 
 
-clickButton = (event) => {
+const clickButton = (event) => {
     const eventName = event.target.name
-    this.setState((prev) => {
-        return {[eventName]: prev[eventName] + 1}
-    })
+
+    switch (eventName){
+      case 'good':
+        console.log('pressed good')
+        setGood(good + 1)
+        break
+      case 'bad':
+        console.log('pressed bad')
+        setBad(bad + 1)
+        break
+      case 'neutral':
+        console.log('pressed neutral')
+        setNeutral(neutral + 1)
+        break
+      default: return
+    }
+
+
 }
 
-countTotalFeedback = () => {
-    const {good, neutral, bad} = this.state
+const countTotalFeedback = () => {
     const result = good + neutral + bad
     return result
 }
 
-countPositiveFeedbackPercentage = () => {
-    const result = Math.round(this.state.good / (this.countTotalFeedback()) * 100);
+const countPositiveFeedbackPercentage = () => {
+    const result = Math.round(good / (countTotalFeedback()) * 100);
     return result
 }
 
 
-render(){
+
     return(
     <>
         <Section sectionName = 'Please leave feedback'>
-          <FeedbackOptions onClick = {this.clickButton} />
+          <FeedbackOptions onClick = {clickButton} />
         </Section>
         <Section sectionName = 'Statistics'>
-          {!this.countTotalFeedback() ? <p>There is no feedback</p> : <Statistics 
-            good = {this.state.good} 
-            neutral = {this.state.neutral}
-            bad = {this.state.bad}
-            total = {this.countTotalFeedback()}
-            positivePercentage = {this.countPositiveFeedbackPercentage()}
+          {!countTotalFeedback() ? <p>There is no feedback</p> : <Statistics 
+            good = {good} 
+            neutral = {neutral}
+            bad = {bad}
+            total = {countTotalFeedback()}
+            positivePercentage = {countPositiveFeedbackPercentage()}
           />}
 
 
         </Section>
       </>
     )
-}
+
 }
 
 export default App;
